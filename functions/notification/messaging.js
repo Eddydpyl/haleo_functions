6cf771ffc66d3242fc390e1d670a7084a050c0b4
Eduardo del Palacio Lirola <eddydpyl@gmail.com>
2019-12-20 13:20:40 +0100

    Fix notifications

--- functions/notification/messaging.js
+++ functions/notification/messaging.js
@@ -10,23 +10,14 @@ exports.sendMessages = function({topic, title, body, type, action, key}) {
             type: type.toString(),
             action: action.toString(),
             key: key,
+            click_action:"FLUTTER_NOTIFICATION_CLICK",
         },
-        android: {
-            data: {
-                click_action:"FLUTTER_NOTIFICATION_CLICK",
-            }
-        },
-        apns: {
-            headers: {
-                "apns-priority": "10",
-            },
-        },
-        webpush: {
-            headers: {
-                Urgency: "high",
-            },
-        }
     };
 
-    return admin.messaging().sendToTopic(topic, message);
+    const options = {
+        priority: "high",
+        contentAvailable: true,
+    };
+
+    return admin.messaging().sendToTopic(topic, message, options);
 };
